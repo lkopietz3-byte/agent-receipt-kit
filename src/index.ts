ac5a42cd6da0314368dbaf03320c301ef9eef468
Lucas <lkopietz3@gmail.com>
2026-08-03 01:13:16 -0500

    Export evidence envelope integrity APIs

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,25 @@ export type {
   ReceiptResult,
 } from './types.js'
 
+export type {
+  JsonPrimitive,
+  JsonValue,
+  EvidenceEnvelopeIntegrity,
+  EvidenceEnvelope,
+  EvidenceEnvelopeVerification,
+  IssueEvidenceEnvelopeOptions,
+} from './integrity.js'
+
+export {
+  EVIDENCE_ENVELOPE_VERSION,
+  EVIDENCE_CANONICALIZATION,
+  EVIDENCE_DIGEST_ALGORITHM,
+  canonicalizeJson,
+  sha256Hex,
+  issueEvidenceEnvelope,
+  verifyEvidenceEnvelope,
+} from './integrity.js'
+
 export { issuePacket } from './packet.js'
 export { verifyReceipt } from './receipt.js'
 export { createRefutationTrail } from './trail.js'
